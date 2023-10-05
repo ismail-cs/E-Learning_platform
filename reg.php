e788a30bc6b09bff81fde648a035a1791573bac8
pantho <panthofdh97@gmail.com>
2023-10-05 18:52:35 +0600

    Update Login Page Background

---
+++ reg.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <title> Lab Task 25 sep 23 </title>
+        <h1> Registration </h1>
+    </head>
+
+
+
+    <style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 10px 22px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  margin: 4px 2px;
+  cursor: pointer;
+  position: relative;
+  border-radius: 12px;
+}
+
+
+
+
+
+</style>
+   
+       
+
+<body >
+            
+
+                <fieldset >
+              
+                        <form method="get" action="ajax.php">
+
+                        First Name: <input type="text" name="fname"> <br>
+
+                        <hr style="width:50%; text-align: left; margin-left:0">
+                
+                        Last Name: <input type="text" name="lname"> <br>
+
+                        <hr style="width:50%; text-align: left; margin-left:0">
+                
+                        Email: <input type="email" name="ml"> <br>
+
+                        <hr style="width:50%; text-align: left; margin-left:0">
+                
+                        Username: <input type="text" name="uname"> <br>
+
+                        <hr style="width:50%; text-align: left; margin-left:0">
+                
+                        Password: <input type="password" name="pass"> <br>
+
+                        <hr style="width:50%; text-align: left; margin-left:0">
+                        
+                        
+                           
+                                <button class="button"> sign up </button>
+                           
+                        
+                    </form>
+                   
+                </fieldset>
+                
+</body>       
+        
+</html>
